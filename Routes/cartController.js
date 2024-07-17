bff0f2db519092aeb7fef759470c939d38810b80
Sukriti <suk.07.march@gmail.com>
2024-07-17 17:43:35 +0530

    finalchanges

--- Routes/cartController.js
+++ Routes/cartController.js
@@ -2,7 +2,8 @@ const Cart = require("../Models/CartItem");
 const User = require("../Models/Users");
 
 exports.addItemToCart = async (req, res) => {
-  const { userId, productId, price, quantity } = req.body;
+  const { userId, productId, name, description, image, price, quantity } =
+    req.body;
 
   try {
     let cart = await Cart.findOne({ userId });
@@ -10,18 +11,20 @@ exports.addItemToCart = async (req, res) => {
       cart = new Cart({ userId, items: [] });
     }
 
-    const itemIndex = cart.items.findIndex(item => item.productId === productId);
+    const itemIndex = cart.items.findIndex(
+      (item) => item.productId === productId
+    );
     if (itemIndex > -1) {
       cart.items[itemIndex].quantity += quantity;
     } else {
-      cart.items.push({ productId, price, quantity });
+      cart.items.push({ productId, name, description, image, price, quantity });
     }
 
     await cart.save();
     res.status(200).json(cart);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Server Error' });
+    res.status(500).json({ error: "Server Error" });
   }
 };
 
@@ -31,15 +34,15 @@ exports.removeItemFromCart = async (req, res) => {
   try {
     const cart = await Cart.findOne({ userId });
     if (!cart) {
-      return res.status(400).json({ msg: 'Cart not found' });
+      return res.status(400).json({ msg: "Cart not found" });
     }
 
-    cart.items = cart.items.filter(item => item.productId !== productId);
+    cart.items = cart.items.filter((item) => item.productId !== productId);
     await cart.save();
     res.status(200).json(cart);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Server Error' });
+    res.status(500).json({ error: "Server Error" });
   }
 };
 
@@ -49,11 +52,11 @@ exports.getCart = async (req, res) => {
   try {
     const cart = await Cart.findOne({ userId });
     if (!cart) {
-      return res.status(400).json({ msg: 'Cart not found' });
+      return res.status(400).json({ msg: "Cart not found" });
     }
     res.status(200).json(cart);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Server Error' });
+    res.status(500).json({ error: "Server Error" });
   }
 };
